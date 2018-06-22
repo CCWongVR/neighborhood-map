5d8ae72305209ca8cf2669a519fe85b4fe656ce6
Colin Wong <36051844+CCWongVR@users.noreply.github.com>
2018-06-22 11:56:10 -0400

    Update restaurants.js

--- Neighborhood Map/js/restaurants.js
+++ Neighborhood Map/js/restaurants.js
@@ -1,54 +1,63 @@
-// Marker Data File
-
-var Restaurants = [
-    { 
-        title: 'Buffet Star', 
-        location: { lat: 42.097379, lng: -75.976031 },
-        category: (1)
+// Marker Data
+restaurants = [
+        {
+            title: 'Buffet Star',
+            location: { lat: 42.097379, lng: -75.976031 },
+            category: 1,
+            FS_id: '4be34c5d1dd22d7f5a4c96bd'
         },
-    { 
-        title: "Wok N' Roll", 
-        location: { lat: 42.094491, lng: -75.976049 },
-        category: (1, 2)
+        {
+            title: "Wok N' Roll",
+            location: { lat: 42.094491, lng: -75.976049 },
+            category: 2,
+            FS_id: '534d73e1498e3548877088e7'
         },
-    { 
-        title: 'Great China Buffet', 
-        location: { lat: 42.123179, lng: -75.970286 },
-        category: (1, 3)
+        {
+            title: 'Great China Buffet',
+            location: { lat: 42.123179, lng: -75.970286 },
+            category: 3,
+            FS_id: '4c4e1adcdb2c20a1aacbde73'
         },
-    { 
-        title: 'China Lake', 
-        location: { lat: 42.106777, lng: -76.022672 },
-        category: 2
+        {
+            title: 'China Lake',
+            location: { lat: 42.106777, lng: -76.022672 },
+            category: 2,
+            FS_id: '4c4b455d712ac928942fac6c'
         },
-    { 
-        title: 'A Plus Sushi', 
-        location: { lat: 42.105558, lng: -75.924834 },
-        category: (1, 3, 4)
+        {
+            title: 'A Plus Sushi',
+            location: { lat: 42.105558, lng: -75.924834 },
+            category:  4,
+            FS_id: '4f8e1aace4b0eab6eeb7819a'
         },
-    { 
-        title: 'China Wok East', 
-        location: { lat: 42.106188, lng: -75.888968 },
-        category: 1
+        {
+            title: 'China Wok East',
+            location: { lat: 42.106188, lng: -75.888968 },
+            category: 1,
+            FS_id: '4ca3692fd7c3370421aea062'
         },
-    { 
-        title: 'New Century', 
-        location: { lat: 42.115167, lng: -75.955708 },
-        category: (1, 3)
+        {
+            title: 'New Century',
+            location: { lat: 42.115167, lng: -75.955708 },
+            category: 3,
+            FS_id: '4c816a1847cc224b521a7c9f'
         },
-    { 
-        title: 'China One', 
-        location: { lat: 42.098503, lng: -75.913619 },
-        category: (1)
+        {
+            title: 'China One',
+            location: { lat: 42.098503, lng: -75.913619 },
+            category: 1,
+            FS_id: '4c0e603f98102d7faa83e306'
         },
-    { 
-        title: 'Fu Star', 
-        location: { lat: 42.089609, lng: -75.913927 },
-        category: (1, 2)
+        {
+            title: 'Fu Star',
+            location: { lat: 42.089609, lng: -75.913927 },
+            category: 2,
+            FS_id: '4c2f6136452620a1655d1d0f'
         },
-    { 
-        title: 'Hong Kong Chinese Restaurant', 
-        location: { lat: 42.092155, lng: -75.824931 },
-        category: (1, 4)
+        {
+            title: 'Hong Kong Chinese Restaurant',
+            location: { lat: 42.092155, lng: -75.824931 },
+            category: 4,
+            FS_id: '4fceaf16e4b0191229cd6627'
         }
-];
+    ];
